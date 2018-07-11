ff4d98b043a2ec988d84e45b16c0b02b3d63c45c
BridgeNB <zyq123258789@gmail.com>
2018-07-11 11:27:35 -0700

    vision 1 added

---
+++ Questions.js
@@ -0,0 +1,112 @@
+const data = [
+  {
+    question: 'Which of the following is a disadvantage of using JavaScript?',
+    choices: [
+      `Client-side JavaScript does not allow the reading or writing of files`,
+      'JavaScript can not be used for Networking applications because there is no such support available',
+      'JavaScript doesn\'t have any multithreading or multiprocess capabilities',
+      'All of the above'
+    ],
+    answer: 'All of the above',
+    alt: 'question 1'
+  },
+  {
+    question: 'Which of the following is correct about callbacks?',
+    choices: [
+      'A callback is a plain JavaScript function passed to some method as an argument or option',
+      'Some callbacks are just events, called to give the user a chance to react when a certain state is triggered',
+      'Both of the above',
+      'None of the above'
+    ],
+    answer: 'Both of the above',
+    alt: 'question 2'
+  },
+  {
+    question: "Which build-in method returns the calling string value converted to upper case?",
+    choices: [
+      "toUpperCase()",
+      "toUpper()",
+      "changeCase(case)",
+      "None of the above"
+    ],
+    answer: "toUpperCase()",
+    alt: 'question 3'
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    choices :[
+      'msgBox("Hello world");',
+      'alert("Hello world");',
+      'print("Hello world");',
+      'alertBox("Hello world");'
+    ],
+    answer: 'alert("Hello world");',
+    alt: 'question 4'
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    choices: [
+      'function myFunction(){}',
+      'function: myFunction(){}',
+      'function = myFunction(){}',
+      'func myFunction(){}'
+    ],
+    answer:'function myFunction(){}',
+    alt: 'question 5'
+  },
+  {
+    question: 'Which of the following function of String object returns a number indicating the Unicode value of the character at the given index?',
+    choices: [
+      'charAt()',
+      'charCodeAt()',
+      'concat()',
+      'indexOf()'
+    ],
+    answer:'charCodeAt()',
+    alt: 'question 6'
+  },
+  {
+    question: 'Which of the following function of String object extracts a section of a string and returns a new string?',
+    choices: [
+      'slice()',
+      'split()',
+      'replace()',
+      'search()'
+    ],
+    answer:'slice()',
+    alt: 'question 7'
+  },
+  {
+    question: 'Which of the following function of Array object returns ture if at least one element in this array satisfies the provided testing function?',
+    choices: [
+      'reverse()',
+      'shift()',
+      'slice()',
+      'some()'
+    ],
+    answer:'some()',
+    alt: 'question 8'
+  },
+  {
+    question: 'Which of the following function of Array object creates a new array with all of the elements of this array for which the provided filtering function returns true?',
+    choices: [
+      'concat()',
+      'every()',
+      'filter()',
+      'some()'
+    ],
+    answer:'filter()',
+    alt: 'question 9'
+  },
+  {
+    question: 'Which of the following function of String object returns a string representing the specified object?',
+    choices: [
+      'toLocaleUpperCase()',
+      'toUpperCase()',
+      'toString()',
+      'substring()'
+    ],
+    answer:'toString()',
+    alt: 'question 10'
+  }
+]
---
+++ index.js
@@ -0,0 +1,133 @@
+'use strict';
+let nthQuestion = 0;
+let score = 0;
+
+function startQuiz() {
+  $('.welcome_page').on('click', '.start_button', function(event) {
+    $('.welcome_page').css("display", "none");
+    $('.question_form').css("display", "block");
+  });
+}
+
+function generateQuestion() {
+  return `
+  <div class="question-${nthQuestion}">
+    <h2>${data[nthQuestion].question}</h2>
+    <form>
+      <fieldset>
+        <label class="choices">
+          <input type="radio" value="${data[nthQuestion].choices[0]}" name="answer" required>
+          <span>${data[nthQuestion].choices[0]}</span>
+        </label>
+        <label class="choices">
+          <input type="radio" value="${data[nthQuestion].choices[1]}" name="answer" required>
+          <span>${data[nthQuestion].choices[1]}</span>
+        </label>
+        <label class="choices">
+          <input type="radio" value="${data[nthQuestion].choices[2]}" name="answer" required>
+          <span>${data[nthQuestion].choices[2]}</span>
+        </label>
+        <label class="choices">
+          <input type="radio" value="${data[nthQuestion].choices[3]}" name="answer" required>
+          <span>${data[nthQuestion].choices[3]}</span>
+        </label>
+        <button type="submit" class="submitAnswer">Submit</button>
+      </fieldset>
+    </form>
+  </div>
+  `;
+}
+
+function renderQuestions() {
+  $('.question_form').html(generateQuestion());
+}
+
+function generateEndPage() {
+  return `
+  <div class="end_page">
+    <h2>This is the end of the Javascript quiz!</h1><br>
+    <h3>You final score is <span class="score">${score}</span> out of 10!</h3>
+    <button type="button" class="restart_button">Play again</button>
+  </div>
+  `;
+}
+
+function renderEndPage() {
+  $('.question_form').html(generateEndPage());
+}
+
+function submitAnswer() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    let choice = $('input:checked').val();
+    let answer = `${data[nthQuestion].answer}`;
+    if (choice === answer) {
+      nextIfCorrectAnswer();
+    } else {
+      nextIfWrongAnswer();
+    }
+    upgradeQuestionNumber();
+  })
+}
+
+function upgradeScore() {
+  score++;
+  $('.score').text(score);
+}
+
+function upgradeQuestionNumber() {
+  nthQuestion++;
+  $('.question_number').text(nthQuestion);
+}
+
+function nextIfCorrectAnswer() {
+  let answer = `${data[nthQuestion].answer}`;
+  let realNthQuestion = 1 + parseInt(`${nthQuestion}`);
+  $('.question_form').html(`<div class="correct_answer question_result">
+      <h3>Cheers!!! You got No. ${realNthQuestion} question right!</h3>
+      <h4>Correct Answer indeed is</h4><br>
+      <p>${data[nthQuestion].answer}</p>
+      <button type=button class="next_button">Next</button>
+    </div>`);
+  upgradeScore();
+}
+
+function nextIfWrongAnswer() {
+  let answer = `${data[nthQuestion].answer}`;
+  let realNthQuestion = 1 + parseInt(`${nthQuestion}`);
+  $('.question_form').html(`<div class="wrong_answer question_result">
+      <h3>Sorry, you got No. ${realNthQuestion} question wrong!</h3>
+      <h4>Correct Answer should be</h4><br>
+      <p>${data[nthQuestion].answer}</p>
+      <button type=button class="next_button">Next</button>
+    </div>`);
+}
+
+function nextQuestion() {
+  $('.question_form').on('click', '.next_button', function(event) {
+    if (nthQuestion < 10) {
+      renderQuestions();
+      submitAnswer();
+    } else {
+      renderEndPage();
+      restartQuiz();
+    }
+  })
+}
+
+function restartQuiz() {
+  $('.end_page').on('click', '.restart_button', function(event) {
+    score = 0;
+    nthQuestion = 0;
+    location.reload();
+  });
+}
+
+function main() {
+  startQuiz();
+  renderQuestions();
+  submitAnswer();
+  nextQuestion();
+}
+
+$(main);
